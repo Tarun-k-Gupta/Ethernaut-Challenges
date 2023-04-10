e9c8b3b8d190ea5ddd3be4b991b4deec2d0d97cf
Tarun Gupta <tarungupta_er@rediffmail.com>
2023-04-10 19:22:01 +0530

    Create 15-NaughtCoin.sol

---
+++ 15-NaughtCoin.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import 'openzeppelin-contracts-08/token/ERC20/ERC20.sol';
+
+ contract NaughtCoin is ERC20 {
+
+  // string public constant name = 'NaughtCoin';
+  // string public constant symbol = '0x0';
+  // uint public constant decimals = 18;
+  uint public timeLock = block.timestamp + 10 * 365 days;
+  uint256 public INITIAL_SUPPLY;
+  address public player;
+
+  constructor(address _player) 
+  ERC20('NaughtCoin', '0x0') {
+    player = _player;
+    INITIAL_SUPPLY = 1000000 * (10**uint256(decimals()));
+    // _totalSupply = INITIAL_SUPPLY;
+    // _balances[player] = INITIAL_SUPPLY;
+    _mint(player, INITIAL_SUPPLY);
+    emit Transfer(address(0), player, INITIAL_SUPPLY);
+  }
+  
+  function transfer(address _to, uint256 _value) override public lockTokens returns(bool) {
+    super.transfer(_to, _value);
+  }
+
+  // Prevent the initial owner from transferring tokens until the timelock has passed
+  modifier lockTokens() {
+    if (msg.sender == player) {
+      require(block.timestamp > timeLock);
+      _;
+    } else {
+     _;
+    }
+  } 
+} 
+
+//Hack: 
+//The above code is given. Since it inherits ERC20, all the functions are available in NaughtCoin as well. So use approve and transferFrom(since they don't have any 
+//modifiers attached) to transfer the tokens from players account. So just call approve first (which will give the approval to the mentioned address(add1 say) to spend 
+//tokens from player's account) and then call transferFrom to transfer the tokens from player's account to some other account. For transferFrom, msg.sender should be 
+//add1. Thus, add1 calls transferFrom function.
